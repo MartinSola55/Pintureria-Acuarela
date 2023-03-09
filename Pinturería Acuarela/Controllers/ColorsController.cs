ad77e27e5b976d105fef2acc701208399a40dd5b
MartinSola55 <martinrsola55@gmail.com>
2023-03-09 20:44:56 -0300

    Deleted unnecessary controllers and views

--- Pinturería Acuarela/Controllers/ColorsController.cs
+++ Pinturería Acuarela/Controllers/ColorsController.cs
@@ -7,9 +7,11 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using Pinturería_Acuarela;
+using Pinturería_Acuarela.Filter;
 
 namespace Pinturería_Acuarela.Controllers
 {
+    [Admin]
     public class ColorsController : Controller
     {
         private EFModel db = new EFModel();
